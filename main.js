1d8e4022a100cb31a2a93c10d42223ff0e3b4e96
Aaron Thomas <acthomas83@gmail.com>
2020-02-04 21:36:07 -0500

    Added resume page

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function randombg(){
-    var random= Math.floor(Math.random() * 2) + 0;
-    var newImage = [
-                   "url('images/Web-Dev-BG-5.png')",
-                   "url('Web-Dev-BG-4.jpg')",
-                   ];
-    document.getElementById("background-animation").style.backgroundImage=newImage[random];
-  }
+// function randombg(){
+//     var random= Math.floor(Math.random() * 2) + 0;
+//     var newImage = [
+//                    "url('./images/Web-Dev-BG-5.png')",
+//                    "url('./images/Web-Dev-BG-4.jpg')",
+//                    ];
+//     document.getElementById("background-animation").style.backgroundImage=newImage[random];
+//   }
