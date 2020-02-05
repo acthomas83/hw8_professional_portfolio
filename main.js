d2d68d01c2b4b51e246be9b7f4f0c160808597bb
Aaron Thomas <acthomas83@gmail.com>
2020-02-04 20:59:46 -0500

    Corrected centered logo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function randombg(){
     var random= Math.floor(Math.random() * 2) + 0;
     var newImage = [
                    "url('images/Web-Dev-BG-5.png')",
-                   "url('images/background_2.jpg')",
+                   "url('Web-Dev-BG-4.jpg')",
                    ];
     document.getElementById("background-animation").style.backgroundImage=newImage[random];
   }
